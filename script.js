b6272f2d26b4901c4d57b1e1d8bb9f8de7cda0a5
Ариадна Тузина <tz_ardn@mail.ru>
2020-11-29 23:09:51 +0700

    toCheck2

--- script.js
+++ script.js
@@ -6,11 +6,6 @@ function chooseLevel() {
 				levels[i].style.listStyle = 'none';
 			};
 			elem.style.listStyleImage = 'url("img/Rectangle.png")';
-			console.log(elem);
-
-			for (let i=0; i<levels.length; i++) {
-				console.log(levels[i].style.listStyle);
-			};
 		});
 	});
 }
@@ -26,21 +21,28 @@ btnStart.addEventListener('click', startGame);
 function startGame() {
 	const menu = document.getElementById('menu');
 	menu.classList.add('displayNone');
-	if (lvlSimple.style.listStyle !== 'none') {
-		console.log('simple');
+	if ((lvlSimple.style.listStyle !== 'none') & (lvlMedium.style.listStyle === 'none') & (lvlComplex.style.listStyle === 'none')) {
 		const simpleGame = document.querySelector('.simple-game');
 		simpleGame.classList.remove('displayNone');
+		// const random = getRandomNumber(1, 3) - 1;
 	} else {
-		if (lvlMedium.style.listStyle !== 'none') {
-			console.log('Medium');
+		if ((lvlMedium.style.listStyle !== 'none') & (lvlSimple.style.listStyle === 'none') & (lvlComplex.style.listStyle === 'none')) {
 			const mediumGame = document.querySelector('.medium-game');
 			mediumGame.classList.remove('displayNone');
+			// const random = getRandomNumber(4, 9) - 1;
 		} else {
-			const complexGame = document.querySelector('.complex-game');
-			complexGame.classList.remove('displayNone');
-			console.log('complex');
-		}
-	}
+			if ((lvlComplex.style.listStyle !== 'none') & (lvlMedium.style.listStyle === 'none') & (lvlSimple.style.listStyle === 'none')) {
+				const complexGame = document.querySelector('.complex-game');
+				complexGame.classList.remove('displayNone');
+				// const random = getRandomNumber(10, 19) - 1;
+			} 
+			else {
+				alert('Выберите уровень!');
+				menu.classList.remove('displayNone');
+			};
+		};
+	};
+	// return random;
 }
 
 const cards = document.querySelectorAll('.cell');
@@ -53,3 +55,24 @@ function flipTheCard() {
 }
 flipTheCard();
 
+function getRandomNumber (min, max) {
+  const d = max - min + 1;
+  const result = min + Math.floor(Math.random() * d);
+  return result;
+}
+
+console.log(random);
+
+function getRandomCard() {
+	const winningCard = document.createElement('img');
+	winningCard.src = 'img/victory.png';
+	cards[random].addEventListener('click', () => {
+		const back = cards[random].children[1];
+		console.log(back);
+		back.children[0].style.display = 'none';
+		console.log(back.children[0]);
+		back.append(winningCard);
+		winningCard.classList.add('card');
+	});
+}
+getRandomCard();
